32e64e389ad0e62ae25cf2fa30c228e46c69306b
jonacko <jessonacko@gmail.com>
2022-09-27 12:28:24 +0100

    seed file updated

--- db/seed.sql
+++ db/seed.sql
@@ -1,25 +1,34 @@
-INSERT INTO department (id, department_name)
-VALUES (1, ""),
-       (2," "),
-       (3, ""),
-       (4,""),
-       (5,""),
-       (6,"");
+USE employeelist;
 
-INSERT INTO employee_role (id, title, salary, department_id)
-VALUES (1, " ", , ),
-       (2, " ", , ),
-       (3, "", , ),
-       (4, " ", , ),
-       (5, "", , ),
-       (6, " ", , );
+INSERT INTO department
+(name)
 
+VALUES
+('Sales'),
+('Support'),
+('Marketing'),
+('Development');
 
+INSERT INTO role (title, salary, department_id)
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "", "", , ),
-       (2, "", "", , ),
-       (3, "", "", , ),
-       (4, "", "", , ),
-       (5, "", "", , ),
-       (6, "", "", , );
+VALUES
+('Sales Manager', 60000.60, 1),
+('Sales Associate', 40000.40, 1),
+('Support Manager', 65000.65, 2),
+('Support Associate', 6500.65, 2),
+('Marketing Manager', 70000.70, 3),
+('Marketing Associate', 50000.55, 3),
+('Development Manager', 100000.10, 4),
+('Development Associate', 80000.88, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+
+VALUES
+('James', 'McAvoy', 1, 2),
+('Alan', 'Rickman', 2, 1),
+('John', 'Cussack', 3, 4),
+('Siouxsie', 'Sioux', 4, 3),
+('Anthony', 'Hopkins', 5, 6),
+('Cameron', 'Diaz', 6, 5),
+('Robert', 'Smith', 7, 8),
+('Mads', 'Mikkelsen', 8, 7);
